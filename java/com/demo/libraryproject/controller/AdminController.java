02eee944158495ed3d79c54d6728a9b00958e3a2
Kristina <kristinna96@gmail.com>
2023-01-25 13:22:50 +0100

    tiny changes

--- java/com/demo/libraryproject/controller/AdminController.java
+++ java/com/demo/libraryproject/controller/AdminController.java
@@ -29,7 +29,7 @@ public class AdminController {
     private ReviewService reviewService;
 
     @Bean
-    ReviewService reviewService(){
+    private ReviewService reviewService(){
         return new ReviewServiceImpl();
     }
 
